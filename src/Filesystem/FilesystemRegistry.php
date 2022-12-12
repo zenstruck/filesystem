5c3f5338a60edbb1f70cd2b90cde7cfda4996ee3
Kevin Bond <kevinbond@gmail.com>
2022-12-12 12:48:40 -0500

    add FilesystemRegistry

---
+++ src/Filesystem/FilesystemRegistry.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Zenstruck\Filesystem;
+
+use Psr\Container\ContainerInterface;
+use Zenstruck\Filesystem;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+final class FilesystemRegistry
+{
+    /** @var array<string,Filesystem> */
+    private array $cache = [];
+
+    /**
+     * @param ContainerInterface|array<string,Filesystem> $filesystems
+     */
+    public function __construct(private ContainerInterface|array $filesystems = [])
+    {
+    }
+
+    public function get(string $name): Filesystem
+    {
+        return $this->cache[$name] ??= new LazyFilesystem(fn() => $this->resolveFilesystem($name));
+    }
+
+    public function reset(): void
+    {
+        $this->cache = [];
+    }
+
+    private function resolveFilesystem(string $name): Filesystem
+    {
+        if ($this->filesystems instanceof ContainerInterface) {
+            return $this->filesystems->get($name);
+        }
+
+        return $this->filesystems[$name] ?? throw new \RuntimeException(\sprintf('Filesystem "%s" is not registered.', $name));
+    }
+}
