176b43eaee345bae0b1541369118a8bdc62fc9ed
Kevin Bond <kevinbond@gmail.com>
2023-02-16 14:32:30 -0500

    fix(symfony): make constraints attributes

--- src/Filesystem/Symfony/Validator/PendingFileConstraint.php
+++ src/Filesystem/Symfony/Validator/PendingFileConstraint.php
@@ -16,6 +16,7 @@ use Symfony\Component\Validator\Constraints\File;
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 final class PendingFileConstraint extends File
 {
     public function validatedBy(): string
--- src/Filesystem/Symfony/Validator/PendingImageConstraint.php
+++ src/Filesystem/Symfony/Validator/PendingImageConstraint.php
@@ -16,6 +16,7 @@ use Symfony\Component\Validator\Constraints\Image;
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 final class PendingImageConstraint extends Image
 {
     public function validatedBy(): string
