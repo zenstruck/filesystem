9e8f7d9992e890f572570dd187741bc00b73fbc6
Kevin Bond <kevinbond@gmail.com>
2023-05-03 15:57:42 -0400

    minor(sca): remove ignored line

--- src/Filesystem/Glide/GlideTransformUrlGenerator.php
+++ src/Filesystem/Glide/GlideTransformUrlGenerator.php
@@ -26,7 +26,7 @@ final class GlideTransformUrlGenerator implements TransformUrlGenerator
 
     public function transformUrl(string $path, array|string $filter, Config $config): string
     {
-        $filter = match (true) { // @phpstan-ignore-line https://github.com/phpstan/phpstan/issues/8937
+        $filter = match (true) {
             \is_string($filter) => ['p' => $filter], // is glide "preset"
             \is_array($filter) && !array_is_list($filter) => $filter, // is standard glide parameters
             \is_array($filter) => ['p' => \implode(',', $filter)], // is array of "presets"
