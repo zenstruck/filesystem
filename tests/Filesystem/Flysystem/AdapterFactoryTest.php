08911a2c4f2a2e5f5e1123492d9732cc31022d3c
Kevin Bond <kevinbond@gmail.com>
2023-05-25 12:05:28 -0400

    fix: allow octal permissions in local adapter factory

--- tests/Filesystem/Flysystem/AdapterFactoryTest.php
+++ tests/Filesystem/Flysystem/AdapterFactoryTest.php
@@ -80,12 +80,12 @@ final class AdapterFactoryTest extends TestCase
     public function can_configure_local_adapter_visibility(): void
     {
         $defaultAdapter = AdapterFactory::createAdapter(TEMP_DIR);
-        $customVisibilityAdapter = AdapterFactory::createAdapter(TEMP_DIR.'?visibility[default_for_directories]=public');
+        $customVisibilityAdapter = AdapterFactory::createAdapter(TEMP_DIR.'?visibility[default_for_directories]=public&visibility[dir][public]=0775');
 
         $defaultAdapter->write('dir1/file1.txt', 'content', new Config());
         $customVisibilityAdapter->write('dir2/file2.txt', 'content', new Config());
 
         $this->assertSame('700', \sprintf('%o', \fileperms(TEMP_DIR.'/dir1') & 0777));
-        $this->assertSame('755', \sprintf('%o', \fileperms(TEMP_DIR.'/dir2') & 0777));
+        $this->assertSame('775', \sprintf('%o', \fileperms(TEMP_DIR.'/dir2') & 0777));
     }
 }
